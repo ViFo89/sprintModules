a8b317a6f501e8b5a55329b557cd031e6a558770
Victor <victor_f_@hotmail.com>
2014-08-28 10:02:08 +0200

    Added dgDialog

--- test-app.js
+++ test-app.js
@@ -1,8 +1,5 @@
-﻿var app = angular.module("test-app", []);
+﻿var app = angular.module("test-app", ['ngDialog']);
 
-app.controller("myCtrl", function() {
-    
-});
 
 app.directive('sprintBacklog', function() {
     return {
@@ -11,23 +8,24 @@ app.directive('sprintBacklog', function() {
     }
 });
 
-app.controller("sprintBacklogController", function($scope) {
+app.controller("sprintBacklogController", ['$scope','ngDialog',function($scope, ngDialog) {
     $scope.sprintItems = [
         {
             description: "Story 1",
             done: true,
             link: "story1",
             tasks: [
-            {
-                status: "doing",
-                description: "Gör det här",
-                link: "task 1.1"
-            },
-            {
-                status: "doing",
-                description: "Inte det här",
-                link: "task 1.2"
-            }]
+                {
+                    status: "doing",
+                    description: "Gör det här",
+                    link: "task 1.1"
+                },
+                {
+                    status: "doing",
+                    description: "Inte det här",
+                    link: "task 1.2"
+                }
+            ]
 
         },
         {
@@ -35,52 +33,85 @@ app.controller("sprintBacklogController", function($scope) {
             done: false,
             link: "story2",
             tasks: [
-            {
-                status: "todo",
-                description: "gul",
-                link: "task 2.1"
-            },
-            {
-                status: "done",
-                description: "blå",
-                link: "task 2.2"
-            }]
+                {
+                    status: "todo",
+                    description: "gul",
+                    link: "task 2.1"
+                },
+                {
+                    status: "done",
+                    description: "blå",
+                    link: "task 2.2"
+                }
+            ]
+        },
+        {
+            description: "Story 3",
+            done: false,
+            link: "story3",
+            tasks: [
+                {
+                    status: "doing",
+                    description: "röd",
+                    link: "task 3.1"
+                },
+                {
+                    status: "todo",
+                    description: "svart",
+                    link: "task 3.2"
+                },
+                {
+                    status: "todo",
+                    description: "grön",
+                    link: "task 3.3"
+                },
+                {
+                    status: "todo",
+                    description: "rosa",
+                    link: "task 3.4"
+                }
+            ]
         }
     ];
 
 
-    $scope.showSprintItemModal = false;
     $scope.toggleSprintItemModal = function (index, itemType) {
-        if (index >= 0) {
-            // todo: task description(s), link(s)
-            var currentItems = $scope.sprintItems[index];
-            switch (itemType) {
-                case "story":
-                    // todo
-                    console.log("story");
-                    $scope.chosenItems = [{ description: currentItems.description, link: currentItems.link }];
-                    break;
-                case "task":
-                    // todo
-                    //var tasks = items.tasks;
-                    $scope.chosenItems = currentItems.tasks;
-                    console.log("task");
-                    break;
-                default:
-                    break;
-            }
+
+        // Creates a new scope for the modal
+        var modalScope = $scope.$new();
+        var description, items;
+        
+
+        switch (itemType.type) {
+            case "story":
+                description = $scope.sprintItems[index].description;
+                items = $scope.sprintItems[index].tasks;
+
+                modalScope.content = {description: description,items: items};
+                break;
+            case "task":
+                description = $scope.sprintItems[index].description + ":" + itemType.status;
+                items = $scope.sprintItems[index].tasks;
+                var modalItems = [];
+
+                // Sorting out the tasks with the right status
+                for (var i = 0, len = items.length; i < len; i++)
+                    if (items[i].status === itemType.status) 
+                        modalItems.push(items[i]);
+
+                modalScope.content = { description: description, items: modalItems };
+                break;
         }
-        $scope.showSprintItemModal = !$scope.showSprintItemModal;
-        var top = index * 50;
-        var el = document.getElementById("sprintItemModal");
-        //console.log(angular.element(el));
-
-    }
 
 
+        ngDialog.open({
+            template: 'sprintBacklogDialog.html',
+            className: 'ngdialog-theme-plain',
+            scope: modalScope
+        });
+    };
 
     $scope.storyLimit = 10;
-
 
     $scope.countTasks = function(index,status) {
         var tasks = $scope.sprintItems[index].tasks;
@@ -93,4 +124,5 @@ app.controller("sprintBacklogController", function($scope) {
         return counter;
     };
 
-});
+}]);
+
